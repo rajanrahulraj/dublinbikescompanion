8291a5edc346ed8b25795d93198438d0652c7986
Aarya Prasannan <aaryaprasannan@gmail.com>
2023-02-22 21:56:28 +0000

    Adding weather data and reducing number of data-columns

--- scripts/constants/app_constants.py
+++ scripts/constants/app_constants.py
@@ -7,7 +7,10 @@ config.read('../../config/app_config.conf')
 class DataSource:
     API_KEY = config.get("JCDecaux", "API_KEY")
     CONTRACT = config.get("JCDecaux", "CONTRACT")
+    WEATHER_API_KEY = config.get("JCDecaux", "WEATHER_API_KEY")
     DYNAMIC_DATA_API = f"https://api.jcdecaux.com/vls/v1/stations?contract={CONTRACT}&apiKey={API_KEY}"
+    def make_weather_api(lat, lon):
+        return f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={DataSource.WEATHER_API_KEY}"
 
 
 class DB:
--- scripts/utilities/cronjob.py
+++ scripts/utilities/cronjob.py
@@ -5,10 +5,38 @@ import datetime
 import schedule
 import time
 
-
 from scripts.constants import app_constants
 from scripts.utilities.MySQLUtils import DBUtils
 
+
+def get_weather_info(position):
+    lat = position['lat']
+    lng = position['lng']
+    weather_response = requests.get(app_constants.DataSource.make_weather_api(lat, lng))
+    weather_data = weather_response.text
+    weather_dict = json.loads(weather_data)
+    current_weather = weather_dict['weather'][0]
+    current_temp = weather_dict['main']
+    current_weather.update(current_temp)
+    return json.dumps(current_weather)
+
+def getStationData(data_dict):
+    station_data = []
+    for station in data_dict:
+        station_values = []
+        station_values.append(station['number'])
+        station_values.append(station['banking'])
+        station_values.append(station['bonus'])
+        station_values.append(station['bike_stands'])
+        station_values.append(station['available_bike_stands'])
+        station_values.append(station['available_bikes'])
+        station_values.append(station['status'])
+        station_values.append(station['last_update'])
+        weather = get_weather_info(station['position'])
+        station_values.append(weather)
+        station_data.append(station_values)
+
+    return station_data
 
 def fetch_data():
     try:
@@ -16,21 +44,18 @@ def fetch_data():
         if response.status_code == 200:
             data = response.text
             data_dict = json.loads(data)
-            query = "INSERT INTO `dublinbikes`.`apidata` (`number`,`contract_name`,`name`,`address`,`position`,`banking`," \
-                    "`bonus`,`bike_stands`,`available_bike_stands`,`available_bikes`,`status`,`last_update`, " \
-                    "`data_fetch_time`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW()); "
-            values = [list(val.values()) for val in data_dict]
-            for val in values:
-                for index in range(len(val)):
-                    if type(val[index]) == dict:
-                        val[index] = json.dumps(val[index])
-            DBUtils().insert_multiple_rows(query, values)
+            station_data = getStationData(data_dict)
+
+            query = "INSERT INTO `dublinbikes`.`apidata` (`number`,`banking`," \
+                    "`bonus`,`bike_stands`,`available_bike_stands`,`available_bikes`,`status`,`last_update`,`weather`, " \
+                    "`data_fetch_time`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW()); "
+            DBUtils().insert_multiple_rows(query, station_data)
     except Exception as e:
         print(e)
 
 
 # fetch_data()
-schedule.every(2).minutes.do(fetch_data)
+schedule.every(5).minutes.do(fetch_data)
 
 while True:
     schedule.run_pending()
