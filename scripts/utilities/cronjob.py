c5368e8169683b4f17f3a93acbbb7d4514d8c1c5
Rahul Raj <118106534+rajanrahulraj@users.noreply.github.com>
2023-02-22 14:48:31 +0000

    scheduled data insert task

--- scripts/utilities/cronjob.py
+++ scripts/utilities/cronjob.py
@@ -7,20 +7,30 @@ import time
 
 
 from scripts.constants import app_constants
+from scripts.utilities.MySQLUtils import DBUtils
 
 
 def fetch_data():
-    response = requests.get(app_constants.DataSource.DYNAMIC_DATA_API)
-    if response.status_code == 200:
-        data = response.text
-        # TO-DO: Remove the sample code to insert into db instead of writing to file
-        with open(f"../../data/api_data_{datetime.datetime.now().timestamp()}", "w") as f:
-            f.write(data)
-        data_dict = json.loads(data)
-        print(data_dict)
+    try:
+        response = requests.get(app_constants.DataSource.DYNAMIC_DATA_API)
+        if response.status_code == 200:
+            data = response.text
+            data_dict = json.loads(data)
+            query = "INSERT INTO `dublinbikes`.`apidata` (`number`,`contract_name`,`name`,`address`,`position`,`banking`," \
+                    "`bonus`,`bike_stands`,`available_bike_stands`,`available_bikes`,`status`,`last_update`, " \
+                    "`data_fetch_time`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW()); "
+            values = [list(val.values()) for val in data_dict]
+            for val in values:
+                for index in range(len(val)):
+                    if type(val[index]) == dict:
+                        val[index] = json.dumps(val[index])
+            DBUtils().insert_multiple_rows(query, values)
+    except Exception as e:
+        print(e)
 
 
-schedule.every(1).minutes.do(fetch_data)
+# fetch_data()
+schedule.every(2).minutes.do(fetch_data)
 
 while True:
     schedule.run_pending()
